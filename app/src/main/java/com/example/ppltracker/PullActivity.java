9217c5b836baee23c79123ed631553013a6aadd9
cedchauv <cedricjchauvet@gmail.com>
2019-09-24 16:21:01 +0900

    Added WIP Weights, Timer on Pull.

--- app/src/main/java/com/example/ppltracker/PullActivity.java
+++ app/src/main/java/com/example/ppltracker/PullActivity.java
@@ -2,13 +2,56 @@ package com.example.ppltracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PullActivity extends AppCompatActivity {
+    private TextView countdownText;
+    private Button countdownButton;
+    private MediaPlayer mp;
 
+    private CountDownTimer countdownTimer;
+    private long msLeft = 1000;
+    private boolean timerOn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pull);
+
+        countdownText = findViewById(R.id.countdownText);
+        countdownButton = findViewById(R.id.restButton);
+        mp = MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+    }
+    public void toggleTimer(View view){
+        if (timerOn){
+            stopTimer();
+        }
+        else{
+            startTimer();
+        }
+    }
+    private void startTimer(){
+        timerOn = true;
+        countdownTimer = new CountDownTimer(msLeft, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                countdownText.setText("" +millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                mp.start();
+            }
+        }.start();
+    }
+    private void stopTimer(){
+        timerOn = false;
+        countdownTimer.cancel();
+
     }
 }
--- app/src/main/java/com/example/ppltracker/WeightsActivity.java
+++ app/src/main/java/com/example/ppltracker/WeightsActivity.java
@@ -1,14 +1,24 @@
 package com.example.ppltracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WeightsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weights);
+
+
     }
+
 }
